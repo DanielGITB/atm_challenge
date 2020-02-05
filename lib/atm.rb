ffdea4480064b5ae27b37e7f9d8ce19c5ecdb045
Hunter <hmvitous@yahoo.com>
2020-02-05 09:21:43 +0100

    Changed atm card date so now the tests pass

--- lib/atm.rb
+++ lib/atm.rb
@@ -11,9 +11,9 @@ class Atm
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds in account', date: Date.today }
         when insufficient_funds_in_atm?(amount)
-            { status: false, message: "insufficient funds in ATM", date: Date.today }
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
         when incorrect_pin?(pin_code, account.pin_code)
-            { status: false, message: "wrong pin", date: Date.today}
+            { status: false, message: 'wrong pin', date: Date.today}
         when card_expired?(account.exp_date)
             { status: false, message: 'card expired', date: Date.today }
         else
