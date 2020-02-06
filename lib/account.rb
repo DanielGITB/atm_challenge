830e5d6cc9c97f34c556880c732c44bf45766e3d
Hunter <hmvitous@yahoo.com>
2020-02-06 10:03:12 +0100

    Fixing account class methods

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,15 @@
 class Account
+    
+    attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
+    
+    STANDARD_VALIDITY_YRS = 5
 
-    def initialize
-        pin_code = rand(1000..9999)
+    def initialize(attrs ={})
+        @pin_code = rand(1000..9999)
+        @exp_date = set_expire_date()
     end
 
     
-    STANDARD_VALIDITY_YRS = 5
 
     def set_expire_date
         Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%Y')
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,11 @@
 require './lib/account.rb'
+require './lib/atm.rb'
 require 'date'
 
 
 describe Account do
-    let(:account) { instance_double('Person', pin_code: '1234', exp_date: '05/20', account_status: :active) }
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '05/20', account_status: :active) }
 
-    before do
-        allow(account).to receive(:balance).and_return(100)
-        allow(account).to receive(:balance=)
-    end
 
     it 'check the length of a number' do
         number = rand(1000..9999)
