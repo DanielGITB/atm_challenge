ba3b11dee7e5883bddf67772039d401bc8d3ef62
Hunter <hmvitous@yahoo.com>
2020-02-06 14:46:15 +0100

    Adding person specs. Withdraw/deposit

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -34,8 +34,33 @@ describe Person do
         let(:atm) { Atm.new }
         before { subject.create_account }
         it 'can deposit funds' do
-            expect(subject.deposit(100)).to be_truthy #?
+            expect(subject.deposit(100)).to be_truthy
         end
+
+        it 'funds are added to the account - deducted from cash' do
+            subject.cash = 100
+            subject.deposit(100)
+            expect(subject.account.balance).to be 100 #keep track on this
+            expect(subject.cash).to be 0
+        end
+    end
+
+    it 'can withdraw funds' do
+        command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm) }
+        expect(command.call).to be_truthy
+    end
+
+    it 'withdraw is expected to raise an error if no ATM is passed in' do
+        command = lambda { subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account) }
+        expect { command.call }.to raise_error 'An ATM is required'
+    end
+
+    it 'funds are added to cash - deducted from account balance' do
+        subject.cash = 100
+        subject.deposit(100)
+        subject.withdraw(amount: 100, pin: subject.account.pin_code, account: subject.account, atm: atm)
+        expect(subject.account.balance).to be 0
+        expect(subject.cash).to be 100
     end
 
     describe 'can not manage funds if not account been created' do
@@ -44,6 +69,7 @@ describe Person do
         
         end
     end
+
 
 
 
