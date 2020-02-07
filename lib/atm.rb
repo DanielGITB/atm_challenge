31ec6bd845c217061dd21338b78681990ef6a7f5
Daniel B <1996.daniel.bryant@gmail.com>
2020-02-07 10:23:43 +0100

    s

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,14 +5,21 @@ class Atm
         @funds = 1000
     end
 
+    def card_expired?(exp_date)
+        Date.strptime(exp_date, '%m/%y') < Date.today
+    end
+
     def withdraw(amount, pin_code, account)
         case 
+
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds in account', date: Date.today }
         when insufficient_funds_in_atm?(amount)
             { status: false, message: "insufficient funds in ATM", date: Date.today}
         when incorrect_pin?(pin_code, account.pin_code)
             { status: false, message: "wrong pin", date: Date.today}
+        when card_expired?(account.exp_date)
+            { status: false, message: 'card expired', date: Date.today }
         else
             perform_transaction(amount, account)
         end
